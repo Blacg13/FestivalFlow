989b8a684f3b5619a498af257f0e1556a0595c73
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2024-11-27 14:23:18 +0100

    initiated ExhibitorSearchBar

---
+++ frontend/src/features/ExhibitorSearchBar/ExhibitorSearchBar.jsx
@@ -0,0 +1,6 @@
+import style from './ExhibitorSearchBar.module.css';
+const ExhibitorSearchBar = () => {
+  return <div className={style.exhibitorSearchBar}>ExhibitorSearchBar</div>;
+};
+
+export default ExhibitorSearchBar;
--- frontend/src/pages/ListPage.jsx
+++ frontend/src/pages/ListPage.jsx
@@ -1,11 +1,13 @@
 import Header from '../shared/Header/Header';
 import Footer from '../shared/Footer/Footer';
+import ExhibitorSearchBar from '../features/ExhibitorSearchBar/ExhibitorSearchBar';
 import ExhibitorList from '../features/ExhibitorList/ExhibitorList';
 const List = () => {
   return (
     <>
       <Header />
       <main className='listPage'>
+        {/* <ExhibitorSearchBar /> */}
         <ExhibitorList format={'div'} />
       </main>
       <Footer />
